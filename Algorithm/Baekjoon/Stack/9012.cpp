1bfa6fcd02f49316ce3270bda2900a911a59169a
김지현 <blumen1229@naver.com>
2020-01-17 19:56:23 +0900

    괄호

---
+++ Algorithm/Baekjoon/Stack/9012.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+string check(string s) {
+    int count = 0;
+    for (int i=0; i<s.size(); i++){
+        if(s[i] == '('){
+            count++;
+        }else{
+            count--;
+        }
+    }
+    if(count==0){
+        return "YES";
+    }else {
+        return "NO";
+    }
+}
+int main(void){
+    int t;
+    cin >> t;
+
+    for(int i=t; i>0; i--){
+        string s;
+        cin >> s;
+
+        cout << check(s) << endl;
+    }
+}
