76c2f6d27b83d6640c96b0d54d4590190e39eb3e
김지현 <blumen1229@naver.com>
2020-01-20 18:27:48 +0900

    피보나치 수5

--- Algorithm/Baekjoon/Recursion/10870.cpp
+++ Algorithm/Baekjoon/Recursion/10870.cpp
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <iostream>
 
-using namespace std;
 int fibonacci(int n);
+using namespace std;
 
 int main(void){
-	int N;	
-	cin >> N;
-	
-	cout << fibonacci(N) << endl;
-	
-	return 0;
+    int n;
+    
+    cin >> n;
+    
+    cout << fibonacci(n) << endl;
+    
+    return 0;
 }
 int fibonacci(int n){
-	if(n<=1) { 
-		return n;
-	}else {
-		return fibonacci(n-1) + fibonacci(n-2);	
-	}
+    if(n==0||n==1){
+        return n;
+    }else{
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
 }
-
