06652c6b956ccaefcdd64bc3574da478714937ca
김지현 <blumen1229@naver.com>
2020-01-21 22:59:09 +0900

    하얀 칸

--- Algorithm/Baekjoon/Search/1100.cpp
+++ Algorithm/Baekjoon/Search/1100.cpp
@@ -3,31 +3,18 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    
     char c;
-    int arr[8][8];
     int count = 0;
     
     for(int i=0; i<8; i++){
         for(int j=0; j<8; j++){
             cin >> c;
-            if(c == 'F'){
-                arr[i][j] = 1;
-            }else {
-                arr[i][j] = 0;
+            if(c == 'F' && (i+j)%2 == 0){ //짝수면 흰색
+                count++;
             }
         }
     }
-    
-    for(int i=0; i<8; i++){
-        for(int j=0; j<8; j++){
-            //cout << arr[i][j];
-            if((i+j)%2 == 0 && arr[i][j]==1){ //짝수면 흰색
-                count ++;
-            }
-        }
-    }
-    
     cout << count << "\n";
     return 0;
 }
