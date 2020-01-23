276bd20f10dec55172c8147e8f077f11d4778128
김지현 <blumen1229@naver.com>
2020-01-23 21:56:55 +0900

    거스름돈

---
+++ Algorithm/Baekjoon/Greedy/5585.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int money;
+    int n = 0;
+    
+    cin >> money;
+    
+    money = 1000 - money;
+    
+    int a[6] = {500, 100, 50, 10, 5, 1};
+    
+    for(int i=0; i<6; i++){
+        if(money == 0){
+            break;
+        }
+        if(money < a[i]){
+            continue;
+        }
+        n += money/a[i];
+        money = money - a[i]*(money/a[i]);
+    }
+    
+    cout << n << "\n";
+    return 0;
+}
+
+
+
