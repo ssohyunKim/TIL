6d4b738127345403c9c01c6c39955ceb352d0cb5
김지현 <blumen1229@naver.com>
2020-01-30 22:49:10 +0900

    수 정렬하기

--- Algorithm/Baekjoon/Sort/2750.cpp
+++ Algorithm/Baekjoon/Sort/2750.cpp
@@ -4,6 +4,9 @@
 using namespace std;
 
 int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
     int n,k;
     cin >> n;
 
@@ -17,6 +20,6 @@ int main(void){
     sort(array, array+n);
 
     for(int i=0; i<n; i++){
-        cout << array[i] << endl;
+        cout << array[i] << "\n";
     }
 }
