f730f5b1875c7c67d84d9491e2276a3a6b344740
ssohyunKim <blumen1229@naver.com>
2020-03-14 20:43:06 +0900

    카드2

--- BOJ/Simulation/2164.cpp
+++ BOJ/Simulation/2164.cpp
@@ -16,31 +16,21 @@ int main(int argc, const char * argv[]) {
     
     vector<int> a(N);
     
-    deque<int> dq;
+    queue<int> q;
     
     for(int i=0; i<N; i++){
-        dq.push_back(i+1);
+        q.push(i+1);
     }
     
     for(int i=0; i<N-1; i=i+2){
-        while(dq.size()>1){
-            //first = dq.front();
-            //cout << first <<"\n";
-            dq.pop_front(); // 1. 맨 앞의 원소 pop
+        while(q.size()>1){
+            q.pop(); // 1. 맨 앞의 원소 pop
             
-            int second = dq.front(); // 2. 다음으로 오는 앞의 원소를 pop하고, 맨 뒤에 push
-            //cout << second <<"\n";
-            dq.pop_front();
-            dq.push_back(second);
+            int element = q.front(); // 2. 다음으로 오는 앞의 원소를 pop하고, 맨 뒤에 push
+            q.pop();
+            q.push(element);
         }
     }
-    cout << dq.front();
+    cout << q.front();
     return 0;
 }
-//1 2 3 4
-//2 3 4
-//3 4 2
-//4 2
-//2 4
-//4
-
