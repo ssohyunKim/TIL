a8994b2774ce1eb839ca405ad4fccd49c09c1710
ssohyunKim <blumen1229@naver.com>
2020-03-26 23:09:04 +0900

    부분 문자열

--- BOJ/String/16916.cpp
+++ BOJ/String/16916.cpp
@@ -1,60 +1,22 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
-string s, p;
-char c[1000000];
-int result;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
 
-void check(string s, string p){
-    bool flag = true;
-    
-    int n = (int)s.length();
-    int m = (int)p.length();
-    
-    if(n>=m){
-        for(int i=0; i<n-m ; i++){
-             for(int j=0; j<m; j++){
-                 if(s[i+j] == p[j]){
-                     c[j] = p[j];
-                 }else {
-                     break;
-                 }
-             }
-             // 넣은 문자 비교해서 같은 값인지 체크
-             for(int j=0; j<m; j++){
-                if(c[j] != p[j]){
-                    flag = false;
-                }else{
-                    continue;
-                }
-            }
-            
-            // c배열 초기화
-            memset(c,0,sizeof(c));
-            
-            // flag값이 true이면 빠져나오고 result 출력
-            if(flag){
-                result = 1;
-                return;
-            }
-        }
-    }else{
+	string s, p;
+
+	cin >> s >> p;
+	
+    char* ptr = strstr((char*)s.c_str(), (char*)p.c_str());
+	
+    if (ptr != NULL){
+        cout << 1 << "\n";
+    } else {
         cout << 0 << "\n";
     }
+	return 0;
 }
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    
-    cin >> s;
-    cin >> p;
-
-    check(s, p);
-    
-    cout << result << "\n";
-    
-    return 0;
-}
-
