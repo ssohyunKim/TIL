d398e9a4f3c3e389ea384c063dd869513b37e9de
ssohyunKim <blumen1229@naver.com>
2020-04-11 20:33:10 +0900

    연구소(수정)

--- BOJ/DFSBFS/14502.cpp
+++ BOJ/DFSBFS/14502.cpp
@@ -1,40 +1,52 @@
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <cstring>
 
 using namespace std;
 
 // 1. 벽을 세우는 것
-// 2. 바이러스를 퍼트리는 것
+// 2. 바이러스를 퍼뜨리는 것
 
 int n, m;
 int map[8][8];
-int temp[8][8];
+int ans = 0;
+vector<pair<int, int> > v;
 // 상, 하, 좌, 우
-int dx[4] = { -1, 1, 0, 0 };
-int dy[4] = { 0, 0, -1, 1 };
-int ans;
+int dx[4] = { 0, 0, -1, 1 };
+int dy[4] = { -1, 1, 0, 0 };
 
 void bfs();
+
 // 벽 만들기
 void dfs(int cnt){
-    if(cnt==3){
+    if(cnt == 3){
         bfs();
         return;
     }
     for(int i=0; i<n; i++){
         for(int j=0; j<m; j++){
-            if(temp[i][j]==0){
-                temp[i][j] = 1;
+            if(map[i][j]==0){
+                map[i][j] = 1; 
                 dfs(cnt+1);
-                temp[i][j] = 0;
+                map[i][j] = 0;
             }
         }
     }
 }
 // 바이러스 퍼트리기
 void bfs(){
+    int temp[8][8];
+    memcpy(temp, map, sizeof(map));
     queue<pair<int,int> > q;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(temp[i][j] == 2){
+                q.push(make_pair(i,j));
+            }
+        }
+    }
+
     while(!q.empty()){
         int x = q.front().first;
         int y = q.front().second;
@@ -43,26 +55,25 @@ void bfs(){
         for(int i=0; i<4; i++){
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if(nx>=0 && ny>=0 && nx<n && ny<n){
-                if(map[nx][ny]==0){
-                    map[nx][ny] = 2;
-                    q.push(make_pair(nx,ny));
-                }
+            if(nx<0 || ny<0 || nx>=n || ny>=n) continue;
+            
+            // 벽 세우기
+            if(temp[nx][ny]==0){
+                temp[nx][ny] = 2;
+                q.push(make_pair(nx,ny));
             }
         }
     }
-    // 개수 세기
-    int temp=0;
+    // 안전구역의 개수 세기
+    int safe=0;
     for(int i=0; i<n; i++) {
         for(int j=0; j<m; j++) {
-            if(map[i][j]==0)
-                temp++;
+            if(temp[i][j]==0)
+                safe++;
         }
     }
- 
-    if(temp>ans){
-        ans = temp;
-    }
+    //최대값 계산
+    ans = max(ans, safe);
 }
 int main(int argc, const char *argv[])
 {
@@ -76,16 +87,15 @@ int main(int argc, const char *argv[])
     for(int i=0; i<n; i++){
         for(int j=0; j<m; j++){
             cin >> map[i][j];
-            temp[i][j] = map[i][j];
         }
     }
 
     for(int i=0; i<n; i++){
         for(int j=0; j<m; j++){
-            if(map[i][j]==0){
-                temp[i][j] = 1;
+            if(map[i][j]==0){ 
+                map[i][j] = 1;
                 dfs(1);
-                temp[i][j] = 0;
+                map[i][j] = 0;
             }
         }
     }
