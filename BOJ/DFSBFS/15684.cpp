f310992c83533d44d1d066241e9ef62ab2e1caba
ssohyunKim <blumen1229@naver.com>
2020-04-24 01:09:25 +0900

    사다리조작

--- BOJ/DFSBFS/15684.cpp
+++ BOJ/DFSBFS/15684.cpp
@@ -1,38 +1,51 @@
 //사다리조작
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int n, m, h;
-int line; // 추가한 가로 선 개수
-bool flag;
-int ans;
-int min = 999999;
+int n, m, h, ans;
+int ladder[31][11];
+
+//사다리탈 수 있는지 없는지 체크하기 위한 함수
+bool check(){
+    bool flag = true;
+    for(int i = 1; i <= n; i++){
+        int pos = i; // 가로선 i부터 시작
+
+        for(int j = 0; j <= h; j++){
+            if(ladder[j][pos] == 1){
+                pos++;
+            }else if(ladder[j][pos-1] == 1){
+                pos--;
+            }
+        }
+        if(pos != i){
+            return flag = false;
+        }
+    }
+    return flag;
+}
 
 //사다리에 선 추가하기
-void addLine(int cnt, int k){
-    // 이미 답을 찾은 경우
-    if(flag){ 
+void dfs(int x, int y, int cnt){
+    if(cnt >= ans) return;
+    if(check()){
+        ans = cnt;
         return;
     }
-    // 개수를 다 쓴 경우
-    if(cnt == h){
-        bool possible = true;
-        for(int i=0; i<n; i++){
-            int row = i; // 가로
-            for(int j=0; j<h; j++){
-                if(ladder[j][row])
-                    row++;
-                else if(row > 1 && ladder[j][row])
-                    row--;
+    if(cnt == 3) return;
+    for(int i = y; i <= h; i++){ // 세로
+        for(int j = x; j < n; j++){  // 가로 줄의 마지막은 넣을 수 없다.
+            //선을 그을 수 있으면
+            if(ladder[i][j] == 0 && ladder[i][j-1] == 0 && ladder[i][j+1] == 0){
+                ladder[i][j] = 1;
+                dfs(i, j, cnt+1);
+                ladder[i][j] = 0;
             }
-
-            // i번 세로선의 결과가 i번이 나와야한다.
-            
         }
-        return;
+        x = 1;
     }
-
 }
 int main(int argc, const char *argv[])
 {
@@ -40,30 +53,19 @@ int main(int argc, const char *argv[])
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
-
-
     cin >> n >> m >> h;
 
-    for(int i=0; i<m; i++){
+    for(int i = 0; i < m; i++){
         int a, b;
         cin >> a >> b;
-
-        // a번째에서 b~b+1을 이어주는 사다리
-        ladder[a][b] = true;
-
-        for(int i=0; i<3; i++){
-            line = i;
-
-            addLine(0, 0);
-
-            // 사다리번호가 i번째가 맞으면 그때의 최솟값을 출력
-            if(){
-                ans = min;
-            }else{
-                ans = -1;
-            }
-
-        }
+        // b세로선과 b+1세로선을 연결하는 a번째 가로선이 있다.  
+        ladder[a][b] = 1;
+    }
+    
+    ans = 4;
+    dfs(1, 1, 0);
+    if(ans == 4){
+        ans = -1;
     }
     cout << ans << '\n';
 
