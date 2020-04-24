f6fc4fff2abe5c8f8f99239f46395734d409b478
ssohyunKim <blumen1229@naver.com>
2020-04-24 22:40:50 +0900

    N과 M(4)

--- BOJ/NM/15652.cpp
+++ BOJ/NM/15652.cpp
@@ -1,27 +1,32 @@
 #include <iostream>
 
+#define MAX 9
 using namespace std;
 
 int n, m;
-int num[10];
+int num[MAX];
+
 // 1부터 N까지 자연수 중에서 M개를 고른 수열
 // 같은 수를 여러 번 골라도 된다.
 // 고른 수열은 비내림차순이어야 한다. 
-void dfs(int x, int cnt){
-    //cout << "debug" << x << " " << cnt << "\n";
 
+void dfs(int cnt){
+    //cout << "debug" << x << " " << cnt << "\n";
     if(cnt == m){
-        //출력
-        for(int i = 0; i < m; i++){
-            cout << num[i] << ' ';
+        // 비내림차순인지 체크
+        for(int i=0; i < m-1; i++){
+            if(num[i] > num[i+1]) return;
         }
-        cout << '\n';
+        // 출력
+        for(int i = 0; i < m; i++){
+            cout << num[i] << " ";
+        }
+        cout << "\n";
         return;
     }
-    for(int i = x; i <= n; i++){
-        
+    for(int i = 1; i <= n; i++){
         num[cnt] = i;
-        dfs(i, cnt+1);
+        dfs(cnt+1);
     }    
 }
 int main(){
@@ -30,7 +35,7 @@ int main(){
 
     cin >> n >> m;
 
-    dfs(1, 0);
+    dfs(0);
 
     return 0;
 }
