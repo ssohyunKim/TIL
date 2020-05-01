677560c8ee4e48b6d60c10494a23392b02b8842e
ssohyunKim <blumen1229@naver.com>
2020-05-02 00:07:51 +0900

    미세먼지 안녕

--- BOJ/Simulation/17144.cpp
+++ BOJ/Simulation/17144.cpp
@@ -7,10 +7,10 @@ int r, c, t;
 
 int map[2][50][50];
 
-int ux, uy, dx, dy;
+int up_x, up_y, down_x, down_y;
 
-int dy[4] = {-1, 1, 0, 0};
-int dx[4] = {0, 0, -1, 1};
+int dy[] = {-1, 1, 0, 0};
+int dx[] = {0, 0, -1, 1};
 
 void spread(int cur)
 {
@@ -34,14 +34,14 @@ void spread(int cur)
     {
         for (int x = 0; x < c; x++)
         {
-            int val = map[cur][y][x] / 5; // 확산된 미세먼지의 양
+            int val = (map[cur][y][x] / 5); // 확산된 미세먼지의 양
             int cnt = 0;                  // 실제로 확산된 미세먼지의 양
             for (int dir = 0; dir < 4; dir++)
             {
                 int ny = y + dy[dir];
                 int nx = x + dx[dir];
 
-                if (ny < 0 || ny >= -r || nx < 0 || nx >= c)
+                if (ny < 0 || ny >= r || nx < 0 || nx >= c)
                     continue;
                 if (map[cur][y][x] != -1)
                 {
@@ -51,7 +51,7 @@ void spread(int cur)
             }
             if (map[next][y][x] != -1)
             {
-                map[next][y][x] += (map[cur][y][x] - val);
+                map[next][y][x] += (map[cur][y][x] - cnt);
             }
         }
     }
@@ -59,7 +59,7 @@ void spread(int cur)
 void move(int cur)
 {
     //상단부분
-    for(int y = uy-1; y > 0 ; y--){
+    for(int y = up_y-1; y > 0 ; y--){
         map[cur][y][0] = map[cur][y-1][0]; //위쪽의 미세먼지를 아래쪽으로
     }
 
@@ -67,18 +67,18 @@ void move(int cur)
         map[cur][0][x] = map[cur][0][x+1]; //오른쪽의 미세먼지를 왼쪽으로
     }
     
-    for(int y = 0; y < uy; y++){
+    for(int y = 0; y < up_y; y++){
         map[cur][y][c-1] = map[cur][y+1][c-1]; //아래의 미세먼지를 위로
     }
 
     for(int x = c-1; x > 0; x--){
-        map[cur][uy][x] = map[cur][uy][x-1]; //왼쪽의 미세먼지를 오른쪽으로
+        map[cur][up_y][x] = map[cur][up_y][x-1]; //왼쪽의 미세먼지를 오른쪽으로
     }
 
-    map[cur][uy][1] = 0; //깨끗한 바람
+    map[cur][up_y][1] = 0; //깨끗한 바람
 
     //하단부분
-    for(int y = dy+1; y < r ; y++){
+    for(int y = down_y+1; y < r ; y++){
         map[cur][y][0] = map[cur][y+1][0]; //위쪽의 미세먼지를 아래쪽으로
     }
 
@@ -86,15 +86,15 @@ void move(int cur)
         map[cur][r-1][x] = map[cur][r-1][x+1];
     }
 
-    for(int y = r-1; y< dy; y--){
+    for(int y = r-1; y< down_y; y--){
         map[cur][y][c-1] = map[cur][y-1][c-1];
     }
 
     for(int x = c-1; x > 1; x--){
-        map[cur][dy][x] = map[cur][dy][x-1];
+        map[cur][down_y][x] = map[cur][down_y][x-1];
     }
 
-    map[cur][dy][1] = 0; //깨끗한 바람
+    map[cur][down_y][1] = 0; //깨끗한 바람
 
 }
 int main()
@@ -102,7 +102,7 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
-    uy = -1; // 공기청정기 초기화
+    up_y = -1; // 공기청정기 초기화
     cin >> r >> c >> t;
 
     for (int y = 0; y < r; y++)
@@ -112,15 +112,15 @@ int main()
             cin >> map[0][y][x];
             if (map[0][y][x] == -1)
             {
-                if (uy == -1)
+                if (up_y == -1)
                 {
-                    uy = y;
-                    ux = x;
+                    up_y = y;
+                    up_x = x;
                 }
                 else
                 {
-                    dy = y;
-                    dx = x;
+                    down_y = y;
+                    down_x = x;
                 }
             }
         }
