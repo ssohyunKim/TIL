59c59408ba1c5be344dd66771d6b1473091c4fe5
ssohyunKim <blumen1229@naver.com>
2020-05-03 18:57:53 +0900

    n과m(3) 두번째

---
+++ BOJ/NM/15651_1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+#define MAX 8
+using namespace std;
+
+int n, m;
+int num[MAX];
+int pick[MAX];
+
+void dfs(int cnt)
+{
+    if (cnt == m)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            cout << pick[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        pick[cnt] = num[i];
+        dfs(cnt + 1);
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
+    {
+        num[i] = i + 1;
+    }
+
+    dfs(0);
+
+    return 0;
+}
