37c586ddbc7cc0ff6916dcacd32c6f7db29c8c81
ssohyunKim <blumen1229@naver.com>
2020-05-07 01:18:43 +0900

    [1차] 다트게임

--- PROGRAMMERS/다트게임.cpp
+++ PROGRAMMERS/다트게임.cpp
@@ -9,33 +9,43 @@ int solution(string dartResult) {
     int answer = 0;
     int score[3] = {0, };   //점수배열
     int idx = 0;
+    int cnt = 1;
 
     for(int i=0; i<dartResult.length(); i++){
         char c = dartResult[i];
-
-        if(c >= '0' && c <= '9'){
+        if(cnt == 1){
             score[idx] += c - '0';
-        }
-
-        if(c == 'S'){
-            score[idx] = pow(score[i], 1);
-        }else if(c == 'D'){
-            score[idx] = pow(score[i], 2);
-        }else if(c == 'T'){
-            score[idx] = pow(score[i], 3);
-        }
-
-        if(c == '*'){
-            score[idx] *= 2;
-            if(idx - 1 >= 0){
-                score[idx] *= 2;
+            cnt ++;
+        }else if(cnt == 2){
+            if(c == 'S'){
+                score[idx] = pow(score[idx], 1);
+            }else if(c == 'D'){
+                score[idx] = pow(score[idx], 2);
+            }else if(c == 'T'){
+                score[idx] = pow(score[idx], 3);
+            }else if(c == '0'){ //10인 경우
+                score[idx] = 10;
+                cnt--;
             }
-        }else if(c == '#'){
-            score[idx] *= -1;
+            cnt ++;
         }
-        idx ++;
+        else if(cnt == 3){
+            if (c == '*'){
+                score[idx] *= 2;
+                if(idx -1 >= 0){
+                    score[idx-1] *= 2;
+                }
+            }else if(c == '#'){
+                score[idx] *= (-1);
+            }else { //없는 경우
+                i--;
+            }
+            
+            cnt = 1;
+            idx++;
+        }
     }
-
+    answer = score[0] + score[1] + score[2];
     return answer;
 }
 int main(){
@@ -46,7 +56,7 @@ int main(){
     , "1D#2S*3S", "1T2D3D#", "1D2S3T*"};
 
     for(int i=0; i<7; i++){
-        cout << solution(array[i]) << "\n"; 
+        cout << solution(array[i]) << "\n";
     }
     
     return 0;
