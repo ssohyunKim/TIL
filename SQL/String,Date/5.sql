a92e858986762824b9acd89e283177314cf74356
ssohyunKim <blumen1229@naver.com>
2020-06-08 23:49:34 +0900

    오랜 기간 보호한 동물(2).sql

---
+++ SQL/String,Date/5.sql
@@ -0,0 +1,11 @@
+-- 입양을 간 동물 중 보호 기간이 가장 길었던 동물
+-- 두 마리의 아이디와 이름을 조회하는 SQL문을 작성 
+-- 보호 기간이 긴 순으로 조회
+
+SELECT A.ANIMAL_ID, A.NAME
+FROM ANIMAL_INS AS A LEFT JOIN ANIMAL_OUTS AS B 
+ON A.ANIMAL_ID = B.ANIMAL_ID
+ORDER BY DATEDIFF(B.DATETIME, A.DATETIME) DESC
+LIMIT 2
+
+# DATEDIFF("나중날짜", "이전날짜")
