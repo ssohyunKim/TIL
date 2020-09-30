b7607b4124f89b14f4aadab0ce4d2913716a95e5
ssohyunkim <blumen1229@naver.com>
2020-09-30 23:44:23 +0900

    1946. 신입사원

--- BOJ/Greedy/1946.cpp
+++ BOJ/Greedy/1946.cpp
@@ -1,38 +1,33 @@
 //1946. 신입사원
 #include <iostream>
-#include <map>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+vector<pair<int, int> > v;
+int T, N;
 int main(){
-    int T;
     cin >> T;
-    
-    for(int i=0; i<T; i++){
-        int N;
+    for(int t=0; t<T; t++){
         cin >> N;
         for(int i=0; i<N; i++){
-            map<int, int> m;
-            int answer = 0;
             int a, b;
             cin >> a >> b;
-            m.insert(make_pair(a, b));
-
-            int comp = m.at(1);
-            int val = 0;
-            
-            for(auto iter=m.begin(); iter!=m.end(); iter++){
-                val = iter->second;
-                if(val <= comp) {
-                    answer ++;
-                }
-                comp = val;
-            }
-
-            cout << answer << "\n";
+            v.push_back(make_pair(a, b));
         }
+        sort(v.begin(), v.end());
+
+        int answer = 1;
+        int rank = v.front().second;
+        for(int i=1; i<N; i++){
+            if(v[i].second < rank) {
+                answer ++;
+                rank = v[i].second;
+            }
+        }
+        cout << answer << "\n";
+        v.clear();
     }
-
-
     return 0;
 }
